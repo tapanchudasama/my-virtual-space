1fdd9c29233c057cee6664300f708e3407f70766
tapanchudasama <tapan9740@gmail.com>
2021-10-07 19:42:14 +0530

    add google analytics:

--- gatsby-config.js
+++ gatsby-config.js
@@ -129,5 +129,22 @@ module.exports = {
         }`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "G-RETTTMCQHS",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false,
+        // defaults to false
+        enableWebVitalsTracking: true,
+      },
+    },
   ],
 };

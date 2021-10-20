53dbc1f3e31f529161f83afe80d4d210a7780c68
tapanchudasama <tapan9740@gmail.com>
2021-10-20 19:43:12 +0530

    aria-label

--- src/components/common/SectionHeading.jsx
+++ src/components/common/SectionHeading.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const SectionHeading = ({ children }) => {
   return (
-    <p className="text-2xl md:text-3xl lg:text-4xl py-6 flex space-x-2 font-medium leading-tight font-bold">
+    <h3 className="text-2xl md:text-3xl lg:text-4xl py-6 flex space-x-2 leading-tight font-bold">
       {children}
-    </p>
+    </h3>
   );
 };
 

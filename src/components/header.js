77aec963de779b9e540a34fa685d6fb59487ac1f
Tapan Chudasama <tapan9740@gmail.com>
2022-07-04 00:44:28 +0530

    hcanges

--- src/components/header.js
+++ src/components/header.js
@@ -2,7 +2,7 @@ import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "gatsby";
 import React from "react";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
-import Navigation from "../pages/navigation";
+import Navigation from "./navigation";
 
 const Header = () => {
   const [showNavigation, setShowNavigation] = React.useState(false);
@@ -24,28 +24,30 @@ const Header = () => {
             {/* <div>
               <ToggleTheme />
             </div> */}
-            <Link to="/navigation">
+            <div
+              role="button"
+              aria-label="Toggle Navigation"
+              tabIndex={0}
+              onKeyDown={() => setShowNavigation(true)}
+              onClick={() => setShowNavigation(true)}
+            >
               <HiOutlineMenuAlt3 className="w-8 h-8 lg:w-10 lg:h-10" />
-            </Link>
+            </div>
           </div>
         </div>
       </div>
-      <AnimatePresence exitBeforeEnter>
-        {showNavigation && (
-          <motion.div
-            className="fixed w-full h-screen top-0 z-10"
-            initial={{ x: "100%" }}
-            animate={{ x: "0%" }}
-            exit={{ x: "100%" }}
-            transition={{ mass: 1 }}
-          >
-            <div className="bg-black bg-opacity-30" />
-            <div className="w-full h-full">
-              <Navigation setShowNavigation={setShowNavigation} />
-            </div>
-          </motion.div>
-        )}
-      </AnimatePresence>
+      {showNavigation && (
+        <motion.div
+          className="fixed w-full h-screen top-0 z-10"
+          initial={{ x: "10%" }}
+          animate={{ x: "0%" }}
+        >
+          <div className="bg-black bg-opacity-30" />
+          <div className="w-full h-full">
+            <Navigation setShowNavigation={setShowNavigation} />
+          </div>
+        </motion.div>
+      )}
     </header>
   );
 };

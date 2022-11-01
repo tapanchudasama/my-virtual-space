70e1e3fafc7500f58e401e0ea39bc067f5eea40b
Tapan Chudasama <tapan9740@gmail.com>
2022-11-01 19:03:37 +0530

    updates

--- src/RootWrapper.jsx
+++
@@ -1,7 +0,0 @@
-import { AnimatePresence } from "framer-motion";
-import React from "react";
-import "./styles/global.css";
-
-export const wrapPageElement = ({ element }) => {
-  return <AnimatePresence>{element}</AnimatePresence>;
-};
